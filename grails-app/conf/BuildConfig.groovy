075028d26cd6fbd018171d787451b8fecfce8236
John Lee <nilpoint@hotmail.com>
2014-09-01 10:32:26 +0800

    Add bootstrap plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -66,6 +66,7 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
         runtime ":resources:1.2.7"
+      runtime ":twitter-bootstrap:3.2.0.2"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
